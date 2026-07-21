1d0501f12fe1c48c71a3847c0e55beb920d5e1c4
Aluno <miguelgalott4@gmail.com>
2026-07-20 21:43:43 -0300

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Center(child: Text("Opa Mundo \n CU", textDirection: TextDirection.ltr,
+  style: TextStyle(fontSize: 32))));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text('dads');
+  }
+}
